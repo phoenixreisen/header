28a138866b8c2e3fc774af88d2faf55f7a69b4a3
Fabian Marcus <apps@fabian-marcus.de>
2020-03-03 14:47:16 +0100

    Variablennamen refactored ("tab" statt "newTab")

--- src/header.m.js
+++ src/header.m.js
@@ -3,14 +3,14 @@ import logo from './phx.logo.svg';
 export const Header = {
 
     view: ({attrs}) => {
-        const {version, url, title, newTab} = attrs;
+        const {version, url, title, tab} = attrs;
         const {protocol, host, pathname} = location;
 
         const href = url ? url : `${protocol || ''}//${host}${(pathname && pathname.length > 1) ? pathname:''}`;
 
         return(
             <article class="phx-header">
-                <a href={href} target={newTab || '_self'} title={title || ''}>
+                <a href={href} target={tab || '_self'} title={title || ''}>
                     <picture class="phx-header__logo">
                         <img src={logo} />
                     </picture>
