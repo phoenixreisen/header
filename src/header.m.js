4a6e47aa3d2a3403bb19c075fbf80a3a2ff004d0
Fabian Marcus <apps@fabian-marcus.de>
2020-03-03 14:39:01 +0100

    konfigurierbares "target" für Logoverlinkung

--- src/header.m.js
+++ src/header.m.js
@@ -3,14 +3,14 @@ import logo from './phx.logo.svg';
 export const Header = {
 
     view: ({attrs}) => {
-        const { version, url } = attrs;
-        const { protocol, host, pathname } = location;
+        const {version, url, newTab} = attrs;
+        const {protocol, host, pathname} = location;
 
         const href = url ? url : `${protocol || ''}//${host}${(pathname && pathname.length > 1) ? pathname:''}`;
 
         return(
             <article class="phx-header">
-                <a href={href}>
+                <a href={href} target={newTab || '_self'}>
                     <picture class="phx-header__logo">
                         <img src={logo} />
                     </picture>
